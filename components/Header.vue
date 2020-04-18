deed34af52d5320098173c193756da0cffd62c38
Kana Ohtake <Kana@Kaoh-Macbook.local>
2020-04-18 14:17:17 +0900

    a

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,10 @@
 <template>
 　<div class="head">
     <div class="title">
-        <h1>COVID19 DIAGNOSIS/診断</h1> 
+        <h1>COVID19 DIAGNOSIS/コロナ診断</h1> 
     </div>
     <div class="info">
-        この診断はコロナに観戦しているか簡単に診断するサイトです。しかし、これはプロジェクトのために作成したものなので、内容や情報が間違っています。もし、高い割合で観戦してると診断されていても信じないでください。
+        この診断はコロナに観戦しているか簡単に診断するサイトです。以下の質問の内容・症状が３日以上続いている方はチェックボックスに記入してください。しかし、これはプロジェクトのために作成したものなので、内容や情報が間違っています。もし、高い割合で感染してると診断されていても信じないでください。
     </div>  
   </div> 
 </template>
@@ -18,19 +18,24 @@ export default {
 <style scooped>
 
 .head{
-    height: 50px;
+    height: 180px;
     width: 100%; 
-    background-color: #367588; 
+    background-color: #013c47; 
     color: #B2EBF2; 
+    position: relative; 
+    padding-left: 10px; 
+    padding-right: 10px; 
+    padding-top: 15px; 
 }
 .title{
     font-size: 30px; 
     text-align: center; 
+    color: #B2EBF2; 
     font-family: "Times New Roman", Times, serif;
 }
 
 .info{
-    font-size: 18 px; 
+    font-size: 18px; 
     text-align: center; 
     font-family: "Times New Roman", Times, serif;
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,53 @@
 <template>
-  <div class="site">
+  <div class="full">
     <Header></Header>
+  <div class="site">
     <div class="question">
-      <input type="checkbox" v-model="select1" @change="calc" /> 熱は37.5度以上在りますか？
+      <input type="checkbox" v-model="select1" @change="calc" /> 熱は37.5度以上ある
       {{ select1 }}
     </div>
     <div class="question2">
-      <input type="checkbox" v-model="select2" @change="calc" /> 味覚は在りますか？
+      <input type="checkbox" v-model="select2" @change="calc" /> 匂いの感覚がない
       {{ select2 }}
     </div>
     <div class="question">
-      <input type="checkbox" v-model="select3" @change="calc" /> 嗅覚は在りますか？
+      <input type="checkbox" v-model="select3" @change="calc" /> 味の感覚がない
       {{ select3 }}
     </div>
     <div class="question2">
-      <input type="checkbox" v-model="select4" @change="calc" /> 喉の痛みや咳などの症状は在りますか？
+      <input type="checkbox" v-model="select4" @change="calc" /> 喉の痛みや咳などの症状がある
       {{ select4 }}
     </div>
     <div class="question">
-      <input type="checkbox" v-model="select5" @change="calc" /> 呼吸に問題は在りますか？
+      <input type="checkbox" v-model="select5" @change="calc" /> 呼吸困難を感じている
       {{ select5 }}
     </div>
     <div class="question2">
-      <input type="checkbox" v-model="select6" @change="calc" /> 鼻水などの症状は在りますか？
+      <input type="checkbox" v-model="select6" @change="calc" /> 鼻水などの症状がある
       {{ select6 }}
     </div>
     <div class="question">
-      <input type="checkbox" v-model="select7" @change="calc" /> 胸のあたりに痛みは在りますか？
+      <input type="checkbox" v-model="select7" @change="calc" /> 胸のあたりに痛みがある
       {{ select7 }}
     </div>
     <div class="question2">
-      <input type="checkbox" v-model="select8" @change="calc" /> 腹痛は在りますか？
+      <input type="checkbox" v-model="select8" @change="calc" /> 腹痛がある
       {{ select8 }}
     </div>
     <div class="question">
-      <input type="checkbox" v-model="select9" @change="calc" /> 胃のむかつきや食欲不振はありましたか？
+      <input type="checkbox" v-model="select9" @change="calc" /> 胃のむかつきや食欲不振が続いている
       {{ select9 }}
     </div>
     <div class="question2">
-      <input type="checkbox" v-model="select10" @change="calc" /> 頭痛などの症状が在りますか？
+      <input type="checkbox" v-model="select10" @change="calc" /> 頭痛などの症状がある
       {{ select10 }}
     </div>
     <div class="question">
-      <input type="checkbox" v-model="select11" @change="calc" /> 筋肉痛の症状はありますか？（運動や行動以外の事で）
+      <input type="checkbox" v-model="select11" @change="calc" /> 筋肉痛の症状がある（運動や行動以外の事で）
       {{ select11 }}
     </div>
     <div class="question2">
-      <input type="checkbox" v-model="select12" @change="calc" /> 非常に疲れていたり、エネルギー不足を感じていませんか
+      <input type="checkbox" v-model="select12" @change="calc" /> 非常に疲れていたり、エネルギー不足を感じている
       {{ select12 }}
     </div>
 
@@ -54,25 +55,56 @@
       <button @click="diagnosis" class="button">診断結果</button> 
     </div>
   </div>
+  <div class="prevent">
+    <h2 class="emergency"><strong>茨城県における緊急事態措置等</strong></h2>
+    <ol class="infor">
+      <li>実施期間：2020年４月18日（土）から５月６日（水）まで</li>
+      <li>対象地域：茨城県全域　</li>
+      <li>実施概要：外出自粛や通勤・通学自粛等の要請、複数の者が参加し，密集状態等が発生する恐れのあるイベント等の開催自粛の要請、別紙の施設に対する休業の要請</li>
+      <li>協力金：１事業者 最大30万円（１事業者当たり10万円。事業所を賃借している場合は10万円を加算。複数賃借している場合はさらに10万円を加算）</li>
+    </ol>
+    <h2 class="emergency"><strong>茨城県にお住いの皆様へ</strong></h2>
+    <p class="notice">緊急事態措置では，国の方針に基づき，必要最小限のものとし，以下の３点を要請します。</p>
+    <ul class="information">
+      <li>茨城県にお住いの方に対する外出自粛要請</li>
+      <li>「３つの密」が重なりやすい施設に対する休業要請</li>
+      <li>「３つの密」が重なりやすい施設でのイベントの開催自粛</li>
+    </ul>
+    <h6><strong>＜ポイント＞</strong> </h6>
+      <p class="notice">社会生活を維持する上で必要な施設（医療施設，生活物資販売施設，食事提供施設，交通機関等）については，感染防止対策を講じて事業を継続します。</p>
+      <p class="important" style="border:3px; border-style:solid; border-color:#FF0000; padding: 5px; margin: 7px; "> 食料・医薬品や生活必需品は，これまでどおり店頭で購入できますので，買い占め等で混乱が生じないよう，冷静な購買活動にご協力ください。</p>
+    <h4><strong>休業要請する施設の具体例</strong></h4>
+    <ul class="information">
+      <li>遊技施設：パチンコ店</li>
+      <li>遊興施設等：キャバレー，ナイトクラブ，ダンスホール，スナック（接待を伴うものに限る。），バー（接待を伴うものに限る。），ダーツバー，パブ，性風俗店，デリヘル，アダルトショップ，個室ビデオ店，カラオケボックス，ライブハウス</li>
+      <li>劇場等：劇場，観覧場，プラネタリウム，映画館，演芸場</li>
+      <li>運動・遊技施設 ：スポーツクラブ，ホットヨガ，ヨガスタジオ，マージャン店，ゲームセンター</li>
+    </ul>
+    <h5><strong>休業要請及び協力金に関するお問い合わせはこちら</strong></h5>
+      <p class="support">緊急事態宣言を受けて，事業者の皆様からの休業要請や協力金に関する相談に対応する専用相談窓口を４月１８日（土）から開設します。<br></br> 開設時間 ９時～１７時（土日祝日を含む毎日）電話番号 ０２９ー３０１ー５３７５</p>
+  </div>
+  </div>
 </template>
 
 <script>
     import Header from "~/components/Header"
 export default {
+  name: "Index",
+  components: { Header },
     data(){
        return{
-         select1: true,
-         select2: true,
-         select3: true,
-         select4: true,
-         select5: true,
-         select6: true,
-         select7: true, 
-         select8: true,
-         select9: true,
-         select10: true,
-         select11: true,
-         select12: true,
+         select1: false,
+         select2: false,
+         select3: false,
+         select4: false,
+         select5: false,
+         select6: false, 
+         select7: false, 
+         select8: false, 
+         select9: false,
+         select10: false, 
+         select11: false, 
+         select12: false, 
          point: 0, 
        }
     }, 
@@ -131,9 +163,23 @@ export default {
 
 <style scooped>
 
+.full{
+  height: 100%;
+  width: 100%;
+  display: inline-block; 
+}
+
 .site{
-  width: 100%; 
+  width: 70%; 
   height: 100%; 
+  display: inline-block; 
+  float: left; 
+}
+
+.prevent {
+  width: 30%;
+  height: 100%; 
+  display: inline-block; 
 }
 
 .question{
@@ -161,6 +207,72 @@ export default {
   padding-bottom: 30px; 
   background-color: #B2EBF2; 
   color: #367588; 
+}
+
+.button {
+  color: #367588; 
+}
+
+.wash{
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+  width: 100%;
+}
+
+.wash2{
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+  width: 100%;
+}
+
+.descript{
+  text-align: center; 
+  color: #367588;
+  padding: 7px 5px; 
+  font-size: 15px; 
+}
+
+.emergency{
+  font-size: 25px; 
+  text-align: center;
+  font-family: "Times New Roman", Times, serif;
+  padding: 6px; 
+}
+.infor{
+  text-align: left; 
+  padding-bottom: 5px; 
+  width: 95%;
+  padding-left: 40px; 
+}
+
+.notice{
+   width: 95%;
+   text-align: center; 
+}
+
+.important{
+  border-width: 3px; 
+  border-color: red; 
+  padding: 5px 9px; 
+}
+
+.information{
+  padding: 9px; 
+}
+
+h4 {
+  text-align: center; 
+  padding: 5px; 
+}
+
+h5 {
+  text-align: center; 
+  padding: 5px; 
+}
+.support{
+  padding: 5px; 
 }
 
 </style> 
